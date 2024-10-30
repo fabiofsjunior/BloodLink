9d918ab87bc76a48d435ecf15af66807ef3f83ca
Fábio F S Jr. <59260204+fabiofsjunior@users.noreply.github.com>
2024-10-30 18:33:55 -0300

    Revert "fix tela de login responsive"
    This reverts commit dbe8d3861ab8cfb424cd856dfa11e9621eb5a353.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,6 @@ android {
 dependencies {
 
     implementation ("com.github.bumptech.glide:glide:4.12.0")
-    implementation(libs.play.services.maps)
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
     implementation(libs.androidx.core.ktx)
